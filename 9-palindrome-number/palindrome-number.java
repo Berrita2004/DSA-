fcd86abcbbf12e8399faf7af8842f7780db8e4b1
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-06-04 23:29:46 +0530

    Time: 5 ms (69.87%) | Memory: 44.1 MB (62.67%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n = 0;
+        int og = x;
+        while ( x > 0 ){
+            int last = x %10;
+            x = x /10;
+             n = (n *10) + last ;
+        }
+        if (n == og ){
+            return true ;
+        }
+        else return false;
+    }
+}
