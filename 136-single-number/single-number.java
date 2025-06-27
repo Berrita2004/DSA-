19f109f07c073d9f8b0422910fb37054d150727d
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-06-28 03:47:07 +0530

    Time: 13 ms (18.83%) | Memory: 45.3 MB (86.36%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,17 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int xor = 0 ;
-        for ( int i = 0 ; i < nums.length ; i ++){
-        xor = xor ^ nums[i] ;
+    public int singleNumber(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(arr[i], 0);
+            mpp.put(arr[i], value + 1);
+
         } 
-        return xor ;
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() == 1) {
+                return it.getKey();
+            } 
     }
+    return -1 ; 
+}
 }
