d8c5c24ae765089632f0b20a5d65eb965872c0d2
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-07-06 22:26:53 +0530

    Time: 49 ms (11.86%) | Memory: 45.1 MB (46.81%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,16 @@
 class Solution {
-    // Returns the indices of the two numbers such that they add up to target
-    public int[] twoSum(int[] arr, int target) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                // If the sum is target, return the result immediately
-                if (arr[i] + arr[j] == target) {
-                    return new int[] {i, j}; // Return as soon as found!
-                }
+    public int[] twoSum(int[] nums, int target) {
+        int sum = 0 ; 
+        int n = nums.length;
+        int [] arr = new int [2];
+        for ( int i = 0 ; i < n ; i ++){
+            for ( int j = i + 1; j < n ; j ++)
+            if (nums[i]+ nums[j]== target){
+                arr[0]=i;
+                arr[1]= j ; 
             }
+
         }
-        // If no solution found, return an empty array (or handle as needed)
-        return new int[] {}; // or throw new IllegalArgumentException("No solution");
+        return arr;
     }
 }
