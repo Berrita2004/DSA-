a69a75261b16a40729f70c0081f37d85f46ed72f
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-08-08 03:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (46.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,26 +2,33 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
-
-        // Step 1: find all the rows and columns that need to be zeroed
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+        boolean firstColZero = false ;
+        for ( int i = 0 ; i < m ; i ++){
+            if (matrix[i][0] == 0){
+                firstColZero = true ;
+            }
+            for ( int j = 1 ; j < n ; j ++){
+                if (matrix[i][j]==0){
+                    matrix[i][0] = 0 ;
+                    matrix [0][j] = 0 ;
                 }
             }
         }
-
-        // Step 2: set cells to 0 if their row or column is marked
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
+        for (int i = 1; i < m ; i++){
+            for ( int j = 1 ; j < n ; j++){
+                if ( matrix [i][0] == 0 || matrix [0][j] == 0 ){
+                    matrix [i][j] = 0 ;
                 }
+            }
+        }
+        if (matrix[0][0] ==0){
+            for ( int j = 0 ; j < n ; j++){
+                matrix[0][j] = 0 ; 
+            }
+        }
+        if (firstColZero){
+            for ( int i = 0 ; i < m ; i ++){
+                matrix[i][0] = 0 ;
             }
         }
     }
