15b77081ced76476d641257bf2d6098a06a3226e
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-08-27 01:23:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (66.98%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,20 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // for loop
-        if ( head == null) return head ; 
-         Stack<Integer> st = new Stack<>();
+        if ( head == null) return head ;
+        ListNode prev = null ; 
         ListNode temp = head ;
-        while( temp!= null){
-            st.push(temp.val);
-            temp = temp.next;
+        while (temp != null){
+            ListNode front = temp.next ;
+            temp.next = prev ;
+            prev = temp ;
+            temp = front;
         }
-        temp = head ;
-        while (temp != null ){
-            temp.val = st.pop();
-            temp = temp.next;
-        }
-        return head;
-
+        return prev ;
     }
 }
