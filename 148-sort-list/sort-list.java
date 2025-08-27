1317a49d2643664518886111b2824928842bdc97
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-08-28 02:17:25 +0530

    Time: 9 ms (96.13%) | Memory: 57.1 MB (38.06%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,21 +10,57 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ArrayList <Integer> arr = new ArrayList<>(); // i forgot syntax
-        ListNode temp = head ; 
-        while ( temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-        //Sorting ArrayList → Arrays.sort() is for arrays, not ArrayList.
-       Collections.sort(arr);
-        temp = head ;
-        int i = 0 ; 
-        while( temp != null){
-            temp.val = arr.get(i);//whst the syntax again ?
-            i++;
-            temp = temp.next;
-        }
-        return head;
+        if (head == null || head.next == null) return head;
+        return mergeSort(head);
     }
+        //first find mid 
+        //sort the array like in merge sort 
+        // get back all the joints in order 
+        public ListNode findMid(ListNode head){
+        ListNode fast = head.next ;
+        ListNode slow = head ;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next ;
+            slow = slow.next ;
+        }
+      return   slow;
+
+    }
+    
+    public ListNode mergeSort(ListNode head){
+        if ( head == null || head.next == null) return head;
+        ListNode mid = findMid( head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode left = head ;
+        ListNode lefthead=  mergeSort(left);
+        ListNode righthead = mergeSort(right);
+        return merge2LL(lefthead,righthead);
+
+    }
+    public ListNode merge2LL(ListNode left, ListNode right){
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        while ( left != null && right != null){
+            if (left.val <= right.val){
+                temp.next = left;
+                left=left.next ;
+            }
+            else {
+                temp.next = right;
+                right = right.next ;
+            }
+            temp= temp.next;
+        }
+        if (left != null){
+            temp.next = left;
+        }
+        else {
+            temp.next = right;
+        }
+        return dummyNode.next;
+    }
+
+
+    
 }
