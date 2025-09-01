66e35a86b2e415da4d12695e013b781b0c6f67ac
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-01 19:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (73.11%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,32 +1,19 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-
-        // Edge cases
-        if (n == 1) return 0;
-        if (nums[0] > nums[1]) return 0;
-        if (nums[n - 1] > nums[n - 2]) return n - 1;
-
-        int low = 1, high = n - 2;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            // Check if mid is a peak
-            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
-
-            // If slope is going up → move right
-            if (nums[mid] > nums[mid - 1]) {
-                low = mid + 1;
-            }
-            // Else slope is going down → move left
-            else {
-                high = mid - 1;
-            }
+        if (n==1) return 0;
+       int low = 0 ;
+       int high = n-1; 
+       while (low < high){
+        int mid = (low + high)/2;
+        if (nums[mid]<nums[mid+1]){
+            low = mid +1 ;
         }
-
-        // Shouldn't happen if array has at least one peak
-        return -1;
+        else {
+            high = mid  ;
+        }
+       }
+       return low;
     }
 }
+
