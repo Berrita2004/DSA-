0d6e3fe6e7111883493225ab2f903324ccc3b705
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-03 15:32:55 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (64.54%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,26 +1,26 @@
 class Solution {
     public int myAtoi(String s) {
-        int  n = s.length() ; 
-        int i = 0 ; 
-        int sign = 1 ; 
-        long res = 0 ; 
-        while ( i < n && s.charAt(i)== ' '){
-               i++;
+        int i = 0 ;
+        int n = s.length();
+        int sign = 1;
+        long res = 0;
+        while (i< n && s.charAt(i)==' '){
+            i++;
+        }
+        if (i < n && ( s.charAt(i)=='-' ||  s.charAt(i)=='+')){
+            if (s.charAt(i)=='-'){
+                sign = -1;
             }
-        if (i <n && (s.charAt(i)== '+' || s.charAt(i)== '-')){
-            sign = (s.charAt(i)== '-')?-1 :1;
             i++;
         }
         while (i < n && Character.isDigit(s.charAt(i))){
-            int digit = s.charAt(i)-'0';
-            if(res> (Integer.MAX_VALUE - digit) / 10){
-                return (sign == 1) ?  Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if (res > Integer.MAX_VALUE / 10 || 
+                   (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            res = res * 10 + digit;
+            res = res * 10 +(s.charAt(i)-'0');
             i++;
         }
-           
         return (int)(sign * res);
-    
     }
 }
