6415404ad0d2240b88fb8712a20d6a62404c4bf8
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-03 16:30:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (68.54%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,23 @@
 class Solution {
+    public double findPower(double x , long n ){
+        if (n== 0 ) return 1 ; 
+        double half = findPower(x, n/2);
+        if(n%2==0){
+            return half * half;
+        }
+        else {
+            return half * half * x;
+        }
+    }
+
+
+
     public double myPow(double x, int n) {
-        long N = n;        // use long to handle Integer.MIN_VALUE
+       long N = n;        // use long to handle Integer.MIN_VALUE
         if (N < 0) {       // handle negative powers
             N = -N;
             x = 1 / x;
         }
-
-        double ans = 1;
-        while (N > 0) {
-            if (N % 2 == 1) {      // if current exponent is odd
-                ans = ans * x;
-            }
-            x = x * x;             // square x
-            N = N / 2;             // divide exponent by 2
-        }
-
-        return ans;
+        return findPower(x,n);
     }
 }
