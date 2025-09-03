76616284d8472b955fb2be7381800f7b02425391
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-03 15:50:16 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (47.72%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,26 +1,33 @@
-class Solution {
+class Solution {  // recurrsion 
+    public int helper(String s , int i , int ans , int sign){
+         int n = s.length();
+         
+        if ( i == n ) return ans ;
+        if (!Character.isDigit(s.charAt(i))) return ans ;
+        int digit = s.charAt(i) - '0';
+        if (ans > Integer.MAX_VALUE / 10 || 
+            (ans == Integer.MAX_VALUE / 10 && digit > 7)) {
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+               ans = ans * 10 + digit;
+        return helper(s, i + 1, ans, sign);
+    }
+
+
     public int myAtoi(String s) {
-        int i = 0 ;
         int n = s.length();
+        int ans = 0 ;
         int sign = 1;
-        long res = 0;
-        while (i< n && s.charAt(i)==' '){
+        int i = 0 ; 
+        while ( i < n && s.charAt(i)==' '){
             i++;
         }
-        if (i < n && ( s.charAt(i)=='-' ||  s.charAt(i)=='+')){
-            if (s.charAt(i)=='-'){
+        if ( i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
+            if (s.charAt(i)== '-'){
                 sign = -1;
             }
             i++;
         }
-        while (i < n && Character.isDigit(s.charAt(i))){
-            if (res > Integer.MAX_VALUE / 10 || 
-                   (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            res = res * 10 +(s.charAt(i)-'0');
-            i++;
-        }
-        return (int)(sign * res);
+        return helper(s, i , ans , sign) * sign;
     }
 }
