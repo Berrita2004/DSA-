9b8c09dc7ebb393fcef3be637bd9aba78b618dfa
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-09 22:40:17 +0530

    Time: 2 ms (94.05%) | Memory: 46.8 MB (52.34%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -3,15 +3,8 @@ class Solution {
         int [] ans = new int [n+1];
         //loop goes till the end of n 
         for ( int i = 0 ; i <= n ; i ++){
-            int cnt = 0 ;
-            int x = i ;
-
-            while (x!= 0){
-                cnt += x & 1;
-                x = x >> 1;
-                // use diffrent variable for i 
-            }
-            ans[i] = cnt ;
+          
+            ans[i] = ans [i>>1] +( i & 1);
         }
         return ans ;
     }
