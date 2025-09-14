c7390db68d828be2fa66b37942e6d82d38a2132b
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-14 16:53:26 +0530

    Time: 4 ms (34.92%) | Memory: 43 MB (98.20%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] ans = new int[n1];
         
-        // Build next greater map for nums2
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!st.isEmpty() && st.peek() <= nums2[i]) {
-                st.pop();
+        for (int i = 0; i < n1; i++) {
+            ans[i] = -1; // default if no greater element
+            boolean found = false;
+            for (int j = 0; j < n2; j++) {
+                if (nums2[j] == nums1[i]) {
+                    // start looking for next greater after nums2[j]
+                    found = true;
+                }
+                if (found && j < n2 && nums2[j] > nums1[i]) {
+                    ans[i] = nums2[j];
+                    break;
+                }
             }
-            if (st.isEmpty()) {
-                map.put(nums2[i], -1);
-            } else {
-                map.put(nums2[i], st.peek());
-            }
-            st.push(nums2[i]);
-        }
-        
-        // Fill answer for nums1 using the map
-        int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = map.get(nums1[i]);
         }
         
         return ans;
