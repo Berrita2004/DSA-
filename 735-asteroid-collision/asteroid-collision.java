c364430faa73be15149fc62b0003d551f1c9a528
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-18 14:09:14 +0530

    Time: 8 ms (15.82%) | Memory: 45.5 MB (35.02%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -5,16 +5,19 @@ class Solution {
         for (int i = 0 ; i < asteroids.length; i ++){
             if (asteroids[i]> 0){
                 st.push(asteroids[i]);
+                //positive element
             }
             else {
+                // if the -ve is greater than top element
                 while (!st.isEmpty() && st.peek() > 0 && Math.abs(asteroids[i])> st.peek() ){
                     st.pop();
                 }
             
             if (!st.isEmpty() &&  st.peek()>0 &&  Math.abs(asteroids[i]) == st.peek()) {
                 st.pop();
+                //if dono ki values same hai toh pop kar do 
             }
-            else if (st.empty () || st.peek() <0 ) 
+            else if (st.empty () || st.peek() <0 ) //if its negative element
             {
                 st.push (asteroids[i]);
             }
