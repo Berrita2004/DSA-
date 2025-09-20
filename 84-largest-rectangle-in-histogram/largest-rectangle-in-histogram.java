57d3515ba8514ddfddca528b5fadce9aa88fa9e5
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-20 23:30:52 +0530

    Time: 68 ms (68.94%) | Memory: 57.7 MB (35.67%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,43 +1,22 @@
 class Solution {
-    public int [] nextSmallerElement (int[] arr){
-        int n = arr.length ;
-        Stack <Integer> st = new Stack<>();
-        int [] nse = new int[n];
-        for ( int i = n-1 ; i >= 0 ; i--){
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]){
-                st.pop();
-            }
-            nse[i] = st.isEmpty() ?  n : st.peek();
-            st.push(i);
-        }
-        return nse;
-    }
-
-       public  int [] prevSmallerElement (int[] arr){
-        int n = arr.length ;
-        Stack <Integer> st = new Stack<>();
-        int [] pse = new int[n];
-        for ( int i = 0 ; i < n ; i++){
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]){
-                st.pop();
-            }
-            pse[i] = st.isEmpty() ?  -1 : st.peek();
-            st.push(i);
-        }
-        return pse;
-    }
-
-
-
     public int largestRectangleArea(int[] heights) {
-        int Max_area= 0 ;
-        int[] p = prevSmallerElement(heights);
-        int [] q =  nextSmallerElement(heights);
-        for ( int i = 0 ; i < heights.length ;i ++){
-            int width = q[i] - p[i] - 1;
-           int area = width * heights[i]  ;
-           Max_area = Math.max(area, Max_area);
+        Stack <Integer> st = new Stack <>();
+        int n = heights.length;
+        int maxArea = 0 ;
+
+       for (int i = 0; i <= n; i++) {  
+            int h = (i == n) ? 0 : heights[i];  
+            while (!st.isEmpty() && h < heights[st.peek()]) {  
+                int height = heights[st.pop()];  
+                int left = (st.isEmpty()) ? -1 : st.peek(); 
+                int width = i - left - 1;  
+                int area = height * width;  
+                maxArea = Math.max(maxArea, area);  
+            }
+
+            st.push(i);  
         }
-        return  Max_area;
+
+    return maxArea ;
     }
 }
