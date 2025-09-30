196800744446be42e7391813db0d7d540da7cfb0
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-09-30 12:41:58 +0530

    Time: 50 ms (52.38%) | Memory: 55 MB (75.74%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -7,7 +7,7 @@ class Solution {
         while( r < n ){
             mpp.put(fruits[r], mpp.getOrDefault(fruits[r], 0) +1);
     
-            while ( mpp.size() > 2){
+            if ( mpp.size() > 2){
                 mpp.put(fruits[l], mpp.get(fruits[l]) - 1);
                 if ( mpp.get(fruits[l])== 0){
                 mpp.remove(fruits[l]);
