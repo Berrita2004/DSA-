1f47643c554072a6af73d72cbebccf35cbb19e6e
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-10-04 16:57:38 +0530

    Time: 5 ms (88.38%) | Memory: 45.1 MB (17.52%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,21 +1,21 @@
-
 class Solution {
-     public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0 ; 
-        int n = s.length();
+    public int lengthOfLongestSubstring(String s) {
         int l = 0 ;
-        HashMap <Character, Integer> h = new HashMap<>();
-        for ( int r = 0 ; r < n ; r++){
-            char c = s.charAt(r);
-            if (h.containsKey(c) && h.get(c) >= l){
-                l = h.get(c) +1;
-            }
-        
-            int len = r-l+ 1;
-            maxLength = Math.max(maxLength, len);
-            h.put(c,r);
-        }
-        return maxLength;
-     }
+        int n = s.length();
+        int maxLen = 0;
+        HashMap <Character, Integer> mpp = new HashMap<>();
 
+        for ( int r =  0 ; r < n; r++){
+            char c = s.charAt(r);
+            //kya hashmap mai l ki value pehle se hai ?
+            //kya woh purani hai ya new?
+            if (mpp.containsKey(c) && mpp.get(c) >= l ){
+                l = mpp.get(c) +1 ;
+            }
+            int len = r -l +1;
+            maxLen = Math.max(maxLen,len);
+            mpp.put(c,r);
+        } 
+        return maxLen;
+    }
 }
