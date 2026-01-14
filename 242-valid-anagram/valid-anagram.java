31a0bd23f96a8fc8dddf7f991714c4f3e35951f8
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-01-14 23:37:35 +0530

    Time: 13 ms (28.59%) | Memory: 46.4 MB (36.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         Map <Character , Integer> map = new HashMap <>();
-        
-//map data structure
+
+//maps used
         for (char c : s.toCharArray())
         map.put(c , map.getOrDefault(c , 0)+1);
 
