852052e979dcf066900c4e082f01d7944bbb75e5
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-01-14 23:37:56 +0530

    Time: 14 ms (26.82%) | Memory: 46.7 MB (17.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,11 +4,11 @@ class Solution {
 
 //maps used
         for (char c : s.toCharArray())
-        map.put(c , map.getOrDefault(c , 0)+1);
+        map.put(c,map.getOrDefault(c , 0)+1);
 
         for (char c :t.toCharArray())
         map.put(c, map.getOrDefault(c, 0)-1);
-
+//
         for (var pair : map.entrySet()){
             if (pair.getValue() != 0 ){
                 return false ;
