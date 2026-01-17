287ae8ff02f848d8c34afadaf3a46ca8531d8bd5
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-01-17 13:21:31 +0530

    Time: 25 ms (46.35%) | Memory: 48.9 MB (53.08%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,12 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sum = 0 ; 
-        int count = 0 ; 
+        int sum = 0 ; //prefix sum
+        int count = 0 ; //no. of subarray with criteria
+
+        // freq map
         Map <Integer , Integer> map = new HashMap <>();
         map.put(0,1);
         for ( int num : nums){
            sum += num;
+
+           // If prefix[j] - prefix[i] = k
+        // => prefix[i] = prefix[j] - k
+            // So check how many times (sum - k) appeared before
         count += map.getOrDefault(sum-k,0);
+        //store it 
         map.put(sum,map.getOrDefault(sum, 0)+1);
        
         }
