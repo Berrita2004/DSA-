85d3c7a5703ec6f41c533f061d9be48981230e6d
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-21 01:10:01 +0530

    [435] [Time Beats: 98.54%] [Memory Beats: 38.71%] - LeetPush

--- LeetCode/Non-overlapping-Intervals.java
+++ LeetCode/Non-overlapping-Intervals.java
@@ -1,19 +1,21 @@
 1class Solution {
-2    public int eraseOverlapIntervals(int[][] intervals) {
-3        int n = intervals.length;
-4        //sort by end time , inc 
-5        Arrays.sort(intervals ,(a,b)-> a[1]-b[1]);
-6        int cnt = 1;
-7        int lastEnd = intervals[0][1];
-8        //compare start to lastEnd 
-9        for(int i = 1 ; i < n; i++){
-10            if(intervals[i][0]>=lastEnd){
-11                cnt++;
-12                //change the end 
-13                lastEnd= intervals[i][1];
-14            }
-15        }
-16    //now for min intervals , n - cnt , i messed up here 
-17        return n-cnt;
-18    }
-19}
+2    //tc +> N(logN)+N
+3    //sc = O(1)
+4    public int eraseOverlapIntervals(int[][] intervals) {
+5        int n = intervals.length;
+6        //sort by end time , inc 
+7        Arrays.sort(intervals ,(a,b)-> a[1]-b[1]);
+8        int cnt = 1;
+9        int lastEnd = intervals[0][1];
+10        //compare start to lastEnd 
+11        for(int i = 1 ; i < n; i++){
+12            if(intervals[i][0]>=lastEnd){
+13                cnt++;
+14                //change the end 
+15                lastEnd= intervals[i][1];
+16            }
+17        }
+18    //now for min intervals , n - cnt , i messed up here 
+19        return n-cnt;
+20    }
+21}
