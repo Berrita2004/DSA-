d0c22df0c76b5779f2dfc145a95a41cdd642137d
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-21 12:20:50 +0530

    [678] [Time Beats: 100.00%] [Memory Beats: 51.65%] - LeetPush

--- LeetCode/Valid-Parenthesis-String.java
+++ LeetCode/Valid-Parenthesis-String.java
@@ -1,7 +1,7 @@
 1class Solution {
 2    public boolean checkValidString(String s) {
-3        int low = 0 ; 
-4        int high = 0 ;
+3        int low = 0 ;  //min open brackets
+4        int high = 0 ;// max  open brackets
 5        for ( char ch : s.toCharArray()){
 6            if (ch == '('){
 7                low++;
@@ -12,12 +12,13 @@
 12                high --;
 13            }
 14            else {
-15                low--;
-16                high ++;
+15                low--;// if '*' acts as ')'
+16                high ++;// if '*' acts as '('
 17            }
-18            if (high < 0 ) return false ;
-19            low = Math.max(low, 0);
-20        }
-21        return low == 0 ;
-22    }
-23}
+18            //low and high cannot be negative 
+19            if (high < 0 ) return false ;
+20            low = Math.max(low, 0);
+21        }
+22        return low == 0 ;
+23    }
+24}
