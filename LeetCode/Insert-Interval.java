c467c1b6d6771f9ae769b52960af76e4cbafdd1a
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-02-21 11:23:35 +0530

    [57] [Time Beats: 97.79%] [Memory Beats: 34.94%] - LeetPush

--- LeetCode/Insert-Interval.java
+++ LeetCode/Insert-Interval.java
@@ -1,22 +1,23 @@
 1class Solution {
 2    public int[][] insert(int[][] intervals, int[] newInterval) {
 3        List<int[]> res = new ArrayList<>();
-4        int n = intervals.length;
-5        int i = 0 ;
-6        while ( i < n && intervals[i][1]< newInterval[0]){
-7            res.add(intervals[i]);
-8            i++;
-9        }
-10        while (i <n &&intervals[i][0]<= newInterval[1] ){
-11            newInterval [0]= Math.min (newInterval[0], intervals[i][0]);
-12            newInterval [1] = Math.max (newInterval[1], intervals[i][1]);
-13            i++;
-14        }
-15        res.add(newInterval);
-16        while (i < n){
-17            res.add(intervals[i]);
-18            i++;
-19        }
-20        return res.toArray(new int [res.size()][]);
-21    }
-22}
+4        //tc and sc = o(N), O(N)
+5        int n = intervals.length;
+6        int i = 0 ;
+7        while ( i < n && intervals[i][1]< newInterval[0]){
+8            res.add(intervals[i]);
+9            i++;
+10        }
+11        while (i <n &&intervals[i][0]<= newInterval[1] ){
+12            newInterval [0]= Math.min (newInterval[0], intervals[i][0]);
+13            newInterval [1] = Math.max (newInterval[1], intervals[i][1]);
+14            i++;
+15        }
+16        res.add(newInterval);
+17        while (i < n){
+18            res.add(intervals[i]);
+19            i++;
+20        }
+21        return res.toArray(new int [res.size()][]);
+22    }
+23}
