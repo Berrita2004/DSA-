e2985e8e8ee687d439e62698ce52666d0c530bce
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2026-08-29 00:20:05 +0530

    [3734] [Time Beats: 76.19%] [Memory Beats: 52.38%] - LeetPush

--- SQL/DCP-08-26/3734-Lexicographically-Smallest-Palindromic-Permutation-Greater-Than-Target.java
+++ SQL/DCP-08-26/3734-Lexicographically-Smallest-Palindromic-Permutation-Greater-Than-Target.java
@@ -1,91 +1,189 @@
 class Solution {
-    public String smallestPalindromicPermutation(String s, String target) {
+    public String lexPalindromicPermutation(String s, String target) {
+
         int n = s.length();
-        int[] freq = new int[26];
-        for (char ch : s.toCharArray()) freq[ch - 'a']++;
+        int[] cnt = new int[26];
 
-        int half = n / 2, mid = n % 2;
-        int oddChar = -1, oddCount = 0;
-        for (int c = 0; c < 26; c++) {
-            if (freq[c] % 2 == 1) { oddChar = c; oddCount++; }
+        for (char c : s.toCharArray()) {
+            cnt[c - 'a']++;
         }
 
-        int M = -1;
-        if (mid == 0) {
-            if (oddCount != 0) return "";
-        } else {
-            if (oddCount != 1) return "";
-            M = oddChar;
+        // More than one odd frequency => impossible
+        int odd = 0;
+        char middle = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if ((cnt[i] & 1) == 1) {
+                odd++;
+                middle = (char) ('a' + i);
+            }
         }
 
-        int[] pairCount = new int[26];
-        for (int c = 0; c < 26; c++) pairCount[c] = freq[c] / 2;
-
-        int[] tgt = new int[n];
-        for (int i = 0; i < n; i++) tgt[i] = target.charAt(i) - 'a';
-
-        // ---- Case A: match target's first half exactly ----
-        int[] T = Arrays.copyOfRange(tgt, 0, half);
-        int[] tCount = new int[26];
-        for (int c : T) tCount[c]++;
-
-        if (Arrays.equals(tCount, pairCount)) {
-            String candidate = build(T, mid, M);
-            if (candidate.compareTo(target) > 0) return candidate;
+        if (odd > 1) {
+            return "";
         }
 
-        // ---- Case B: find rightmost position to exceed target ----
-        int[] remaining = pairCount.clone();
-        int bestK = -1;
-        int[] bestState = null;
+        // Number of pairs available for left half
+        int[] pairs = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            pairs[i] = cnt[i] / 2;
+        }
+
+        int half = n / 2;
+
+        /*
+         * We want to find the smallest left half
+         * that is >= target's left half.
+         *
+         * Try to follow target as long as possible.
+         */
+
+        int[] remaining = pairs.clone();
+
+        boolean samePrefixPossible = true;
 
         for (int i = 0; i < half; i++) {
-            int t = tgt[i];
-            for (int c = t + 1; c < 26; c++) {
-                if (remaining[c] > 0) {
-                    bestK = i;
-                    bestState = remaining.clone();
+
+            int c = target.charAt(i) - 'a';
+
+            if (remaining[c] == 0) {
+                samePrefixPossible = false;
+                break;
+            }
+
+            remaining[c]--;
+        }
+
+        /*
+         * Case 1:
+         * We can make target's entire left half.
+         *
+         * Then check whether its palindrome is > target.
+         */
+        if (samePrefixPossible) {
+
+            String left = target.substring(0, half);
+
+            StringBuilder candidate = new StringBuilder();
+
+            candidate.append(left);
+
+            if ((n & 1) == 1) {
+                candidate.append(middle);
+            }
+
+            candidate.append(new StringBuilder(left).reverse());
+
+            if (candidate.toString().compareTo(target) > 0) {
+                return candidate.toString();
+            }
+        }
+
+        /*
+         * Case 2:
+         * Find the RIGHTMOST position where we can
+         * make target[i] larger.
+         *
+         * For every position after i, we restore their
+         * pairs because those positions are no longer
+         * fixed.
+         */
+        int[] used = pairs.clone();
+
+        // Consume target's left half as much as possible.
+        int matched = 0;
+
+        for (int i = 0; i < half; i++) {
+
+            int c = target.charAt(i) - 'a';
+
+            if (used[c] == 0) {
+                break;
+            }
+
+            used[c]--;
+            matched++;
+        }
+
+        /*
+         * We need to search backwards.
+         *
+         * Rebuild the available pairs for each position.
+         * Since n <= 300, O(n * 26) is completely fine.
+         */
+        for (int i = half - 1; i >= 0; i--) {
+
+            /*
+             * Reconstruct remaining pairs after fixing
+             * target[0 ... i-1].
+             */
+            int[] rem = pairs.clone();
+            boolean validPrefix = true;
+
+            for (int j = 0; j < i; j++) {
+
+                int c = target.charAt(j) - 'a';
+
+                if (rem[c] == 0) {
+                    validPrefix = false;
                     break;
                 }
+
+                rem[c]--;
             }
-            if (remaining[t] > 0) {
-                remaining[t]--;
-            } else {
-                break; // can't extend the matched prefix further
+
+            if (!validPrefix) {
+                continue;
+            }
+
+            int current = target.charAt(i) - 'a';
+
+            /*
+             * Increase this position by the smallest
+             * possible character.
+             */
+            for (int next = current + 1; next < 26; next++) {
+
+                if (rem[next] == 0) {
+                    continue;
+                }
+
+                rem[next]--;
+
+                StringBuilder left = new StringBuilder();
+
+                // Same prefix as target
+                left.append(target, 0, i);
+
+                // First greater character
+                left.append((char) ('a' + next));
+
+                // Smallest possible suffix
+                for (int c = 0; c < 26; c++) {
+                    for (int k = 0; k < rem[c]; k++) {
+                        left.append((char) ('a' + c));
+                    }
+                }
+
+                String leftPart = left.toString();
+
+                StringBuilder answer = new StringBuilder();
+
+                answer.append(leftPart);
+
+                if ((n & 1) == 1) {
+                    answer.append(middle);
+                }
+
+                answer.append(
+                    new StringBuilder(leftPart).reverse()
+                );
+
+                return answer.toString();
             }
         }
 
-        if (bestK == -1) return "";
-
-        remaining = bestState;
-        int t = tgt[bestK];
-        int x = -1;
-        for (int c = t + 1; c < 26; c++) {
-            if (remaining[c] > 0) { x = c; break; }
-        }
-        remaining[x]--;
-
-        int[] L = new int[half];
-        for (int i = 0; i < bestK; i++) L[i] = tgt[i];
-        L[bestK] = x;
-
-        int idx = bestK + 1;
-        for (int c = 0; c < 26; c++) {
-            for (int k = 0; k < remaining[c]; k++) {
-                L[idx++] = c;
-            }
-        }
-
-        return build(L, mid, M);
-    }
-
-    private String build(int[] L, int mid, int M) {
-        int half = L.length;
-        int n = half * 2 + mid;
-        char[] res = new char[n];
-        for (int i = 0; i < half; i++) res[i] = (char) (L[i] + 'a');
-        if (mid == 1) res[half] = (char) (M + 'a');
-        for (int i = 0; i < half; i++) res[n - 1 - i] = (char) (L[i] + 'a');
-        return new String(res);
+        return "";
     }
 }
